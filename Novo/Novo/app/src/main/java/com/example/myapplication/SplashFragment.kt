36f34900aca249677297a155006a235c0d20d1a5
cc23632 <cc23632@g.unicamp.br>
2024-06-24 00:40:04 -0300

    a

--- Novo/Novo/app/src/main/java/com/example/myapplication/SplashFragment.kt
+++ Novo/Novo/app/src/main/java/com/example/myapplication/SplashFragment.kt
@@ -3,16 +3,18 @@ package com.example.myapplication
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+<<<<<<< Updated upstream
 import androidx.lifecycle.lifecycleScope
+=======
+import androidx.fragment.app.Fragment
+>>>>>>> Stashed changes
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentSplashBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-
 
 class SplashFragment : Fragment() {
 
@@ -27,6 +29,7 @@ class SplashFragment : Fragment() {
         return _binding!!.root
     }
 
+<<<<<<< Updated upstream
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -38,9 +41,18 @@ class SplashFragment : Fragment() {
     }
 
 
+=======
+>>>>>>> Stashed changes
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        }, 3000) // Delay de 3 segundos
+    }
 }

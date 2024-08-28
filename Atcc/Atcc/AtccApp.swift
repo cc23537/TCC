745a79f7d4b8890412bad71dcbd2ef42727d883d
cc23537 <142462791+cc23537@users.noreply.github.com>
2024-08-28 19:08:41 -0300

    a

---
+++ Atcc/Atcc/AtccApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtccApp.swift
+//  Atcc
+//
+//  Created by RAFAEL ALVES DE OLIVEIRA on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AtccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

5b733e4ed4458f759569e35fb2ea7aee4bf1d4e5
cc23520 <cc23520@g.unicamp.br>
2024-08-30 20:09:41 -0300

    a

--- NovaVersao/app/src/main/java/com/example/appcomida/AddListaDialogFragment.kt
+++ NovaVersao/app/src/main/java/com/example/appcomida/AddListaDialogFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.lifecycleScope
+import com.example.appcomida.api.registerUser
 import com.example.appcomida.databinding.FragmentAddListaDialogBinding
+import kotlinx.coroutines.launch
 
 
 class AddListaDialogFragment : DialogFragment() {
@@ -32,13 +35,21 @@ class AddListaDialogFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnAddC.setOnClickListener {
+            val nomeCompra = binding.edtNomeAddCompra.text.toString()
+            val dataCompra = binding.edtDataCompra.text.toString()
+            val descCompra = binding.edtDescCompra.text.toString()
 
-        binding.btnAddC.setOnClickListener{
-            //adicionar()
-            val NomeCompra = binding.edtNomeAddCompra.text.toString()
-            val DataCompra = binding.edtDataCompra.text.toString()
-            val DescCompra = binding.edtDescCompra.text.toString()
+
+            lifecycleScope.launch {
+                registerUser(nomeCompra, dataCompra, descCompra)
+            }
+
+
+            dismiss()
         }
     }
+
+
 
 }

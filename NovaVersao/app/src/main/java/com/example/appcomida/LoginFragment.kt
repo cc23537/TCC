040dcf78c1ab856e68438a7d6e244b6003f7c24b
cc23537 <142462791+cc23537@users.noreply.github.com>
2024-10-29 20:23:39 -0300

    a

--- NovaVersao/app/src/main/java/com/example/appcomida/LoginFragment.kt
+++ NovaVersao/app/src/main/java/com/example/appcomida/LoginFragment.kt
@@ -93,7 +93,7 @@ class LoginFragment : Fragment() {
             .build()
 
         return Retrofit.Builder()
-            .baseUrl("https://257e-187-106-37-122.ngrok-free.app/")
+            .baseUrl("https://2b1e-187-106-37-122.ngrok-free.app/")
             .client(client)
             .addConverterFactory(ScalarsConverterFactory.create())
             .build()
